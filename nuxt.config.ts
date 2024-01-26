a6303d7c2db66e252f31f117f2c62f26c36df9b5
Gonzalo <espindolamgonzalo@gmail.com>
2024-01-26 07:59:44 -0300

    favicon change and head page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,14 @@ export default defineNuxtConfig({
   css: [
     '~/styles/main.css',
   ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { property: 'og:title', content: 'Gonzalo Espindola - Portfolio' },
+        { property: 'og:description', content: 'Portfolio personal de Gonzalo Espindola' },
+      ]
+    }
+  }
 })
